af567767c99626a0035186c9b271e9da97fa246b
Manish1113 <manish.k@incture.com>
2025-04-08 14:58:56 +0530

    Adding structure of code snipit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+    <>
+      <table>
+        <tr>
+          <th>Name</th>
+        </tr>
+      </table>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Home.jsx
@@ -0,0 +1,9 @@
+class Car extends React.Component {
+    render() {
+        return <h2>Hi, I am a Car! </h2>
+    }
+}
+
+function Cars() {
+    return <h2>Hi, I am a Car! </h2>
+}
